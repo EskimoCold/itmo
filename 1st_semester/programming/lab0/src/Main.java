d59d143b1081d532904f266dc7820d37cbb5ec46
Эдуард Суджян <eskimo0c0ld@gmail.com>
2023-09-19 17:34:52 +0300

    java lab1

--- 1st_semester/programming/lab0/src/Main.java
+++ 1st_semester/programming/lab0/src/Main.java
@@ -2,15 +2,15 @@ import java.lang.Math;
 import java.util.Random;
 
 public class Main {
-    static double firstFormula(double x) {
+    static double firstFormula(float x) {
         return Math.tan(Math.sin(Math.pow(x, (x / (x + 0.75f)))));
     }
 
-    static double secondFormula(double x) {
+    static double secondFormula(float x) {
         return Math.sin((Math.pow(x, x / (x - 0.5))) / 2);
     }
 
-    static double thirdFormula(double x) {
+    static double thirdFormula(float x) {
         return Math.cos(Math.atan(Math.pow((x - 4.5) / 17, 4)));
     }
 
@@ -20,6 +20,7 @@ public class Main {
         float[][] res = new float[6][14];
 
         Random rand = new Random();
+
         byte el = 16;
         for (int i = 0; i < c.length; i++) {
             c[i] = el;
@@ -30,17 +31,30 @@ public class Main {
             x[i] = rand.nextFloat()*17 - 13;
         }
 
+        // calculate main array
         for (int i = 0; i < c.length; i++) {
             for (int j = 0; j < x.length; j++) {
-                // main code here
+                if (c[i] == 16) {
+                    res[i][j] = (float) firstFormula(x[j]);
+                }
 
+                else if (c[i] == 6 || c[i] == 10 || c[i] == 14) {
+                    res[i][j] = (float) secondFormula(x[j]);
+                }
 
+                else {
+                    res[i][j] = (float) thirdFormula(x[j]);
+                }
             }
+        }
+
+        // print array
+        for (int i = 0; i < c.length; i++) {
+            for (int j = 0; j < x.length; j++) {
+                System.out.printf("%.5f ", res[i][j]);
+            }
+
+            System.out.println();
         }
     }
 }
-
-// talk with 3 people; cant now lets at end; picasso ...
-// have a bf? no; negative exp in company;
-// still practise philisophy? i still think a lot; he got a job
-// what animal? cat(good life); didnt get a job, stupid question
