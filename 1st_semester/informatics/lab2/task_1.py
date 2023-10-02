1162eaa81bebd563c78bc018504a1380c6999ff2
Эдуард Суджян <eskimo0c0ld@gmail.com>
2023-10-02 17:39:46 +0300

    inf lab2 1st task

---
+++ 1st_semester/informatics/lab2/task_1.py
@@ -0,0 +1,28 @@
+import re
+
+
+# :</
+
+tests = [
+    "smile face - :) my isu face - :</",
+    "just nothing.",
+    ":/()<>:</::()/.,.?.:</:</",
+    ":<</ ::<<// :::</// :>/ :</",
+    "':</' * 5 = ':</:</:</:</:</'"
+]
+
+answers = [
+    1,
+    0,
+    3,
+    2,
+    6
+]
+    
+pattern = r":</"
+
+if __name__ == "__main__":
+    for i in range(len(tests)):
+        count = len(re.findall(pattern, tests[i]))
+        
+        print(f"test: {tests[i]}\nregex answer: {count}\ntrue answer: {answers[i]}\n")
